cc221ae022984059c427f3a8b4db5c6fc2fcb9fb
QDU-Tracy <sdlqmc@yeah.net>
2018-08-08 19:08:04 +0800

    Footballer Info Alpha Version

---
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+    @RequestMapping("index")
+    public String index() {
+        return "indexMain";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
