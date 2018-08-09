50c1d3869af6d091a6502ef218587aa43d22cf1b
QDU-Tracy <sdlqmc@yeah.net>
2018-08-09 19:18:10 +0800

    Footballer Info Alpha Version

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -1,14 +1,14 @@
 package com.springboot.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @SpringBootApplication
+@MapperScan("com.springboot.demo.mapper")
 public class DemoApplication {
 
     @RequestMapping("index")
